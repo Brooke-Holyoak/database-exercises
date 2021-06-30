aa10351a0159e0d0acc674c1aafe78dd33cf68ff
Brooke-Holyoak <brooke.holyoak24@gmail.com>
2021-06-30 11:49:11 -0500

    adding new file

---
+++ subqueries_exercises.sql
@@ -0,0 +1,14 @@
+
+## Intro: Create a file named subqueries_exercises.sql and craft queries to return the results for the following criteria:
+
+## 1. Find all the current employees with the same hire date as employee 101010 using a sub-query.
+
+## 2. Find all the titles ever held by all current employees with the first name Aamod.
+
+## 3. How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
+
+## 4. Find all the current department managers that are female. List their names in a comment in your code.
+
+## 5. Find all the employees who currently have a higher salary than the companies overall, historical average salary.
+
+## 6. How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?
